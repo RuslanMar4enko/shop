f804bf3616a5c82204bb4ad3eaa638b62e83b9ab
rus-march@yandex.ua <rus-march@yandex.ua>
2017-01-29 22:39:22 +0200

    Create cabinet fix

--- www/controllers/CabinetController.php
+++ www/controllers/CabinetController.php
@@ -5,6 +5,9 @@ class CabinetController
     public function actionIndex()
     {
         $userId = User::checkLogged();
+
+        $user = User::getUserById($userId);
+
         require_once(ROOT . '/views/cabinet/index.php');
 
         return true;
--- www/views/cabinet/index.php
+++ www/views/cabinet/index.php
@@ -6,7 +6,8 @@
     <div class="container">
         <div class="row">
 
-            <h3>Привет, Руслан!</h3>
+
+            <h3>Привет, <?php echo ucfirst($user['name']);?>!</h3>
             <ul>
                 <li><a href="/cabinet/edit">Редактировать данные</a></li>
                 <li><a href="/cabinet/history">Список покупок</a></li>
@@ -15,7 +16,6 @@
         </div>
     </div>
 </section>
-
 
 
 <?php include ROOT . '/views/layouts/footer.php'; ?>
