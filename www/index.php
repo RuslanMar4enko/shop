d2fc6eb4e6f0cdeb5b550ce230a0e6831ab9d3aa
rus-march@yandex.ua <rus-march@yandex.ua>
2017-01-29 21:00:17 +0200

    Create registerAction

--- www/index.php
+++ www/index.php
@@ -9,9 +9,7 @@ error_reporting(E_ALL);
 
 // 2. Подключение файлов системы
 define('ROOT', dirname(__FILE__));
-require_once(ROOT.'/components/Router.php');
 require_once(ROOT.'/components/Autoload.php');
-require_once(ROOT.'/components/Db.php');
 
 
 // 4. Вызов Router
